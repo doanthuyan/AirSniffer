b2d203ac75d0071de02e13c36445b2ab03da6350
Axon Active Vietnam <aavn-github@axonactive.com>
2018-07-30 14:23:54 +0700

    Update Sniffer.ino

--- source/Sniffer/Sniffer.ino
+++ source/Sniffer/Sniffer.ino
@@ -27,6 +27,11 @@ int btnVoltage, refVoltage;
 
 void highInterrupt();
 void showStatus(bool ok);
+bool led_status = false;
+unsigned long lastCall;
+unsigned long lastReadCall;
+bool restarted = true;
+int blink_type = 1; //0: no blink; 1: blink; 2: flash twice; 3: flash three
 
 void setup(){
   //ESP.eraseConfig();
@@ -36,17 +41,18 @@ void setup(){
   Serial.println();
   Serial.println("Hub started");
   pinMode(CONFIG_BTN, INPUT_PULLUP);
-  //pinMode(ERR_PIN, OUTPUT);
+  pinMode(ERR_PIN, OUTPUT);
   pinMode(REF_PIN, OUTPUT);
   //pinMode(OK_PIN, OUTPUT);
  
+  attachInterrupt(CONFIG_BTN, highInterrupt, FALLING);
 
   initSnifferConfig(&hubConfig);
   if(isConfigMode(&hubConfig)){
     setupAP(&hubConfig);
     
   }else{
-    //connectWifi(&hubConfig);
+//    connectWifi(&hubConfig, ERR_PIN);
 
     //start ok
     
@@ -54,8 +60,9 @@ void setup(){
     Serial.print("LIBRARY VERSION: ");
     Serial.println(DHT_LIB_VERSION);
   }
+  lastCall = millis();  
+  lastReadCall = millis();
   
-  attachInterrupt(CONFIG_BTN, highInterrupt, FALLING);
 }
 
 void loop(){
@@ -65,41 +72,74 @@ void loop(){
     ESP.restart();
   }else{
     if(isConfigMode(&hubConfig)){
-      
       handleSmartConfigClient();
-      delay(6000);
+//      delay(6000);
+      if ((millis()-lastCall) > 300) {
+        led_status = !led_status;
+        lastCall = millis();
+      }
+      showStatus(led_status);
     }else{
       
-      readSnifferData();
-      if (isValidAirData(envData)) {
+      if (((millis()-lastReadCall) > 600000) || restarted || (millis()<lastReadCall)) {
+        restarted = false;
+        lastReadCall = millis();
         showStatus(true);
-        if(WiFi.status() != WL_CONNECTED){
-          connectWifi(&hubConfig);
+        readSnifferData();
+        if (isValidAirData(envData)) {
+          showStatus(true);
+          if(WiFi.status() != WL_CONNECTED){
+            connectWifi(&hubConfig);
+          }
           
-        }
-        
-        if(WiFi.status() == WL_CONNECTED){
-          RestProperty restProperty;
-          restProperty.sender_pro= hubConfig.code;
-          restProperty.latitude_pro= hubConfig.latitude;
-          restProperty.longitude_pro= hubConfig.longitude;
-          restProperty.TEMP_SENSOR_pro= TEMP_SENSOR;
-          restProperty.PM_SENSOR_pro= PM_SENSOR;
-          restProperty.mac_str_pro = hubConfig.macStr;
-          bool ok = saveData(envData,restProperty);
-          showStatus(ok);
+          if(WiFi.status() == WL_CONNECTED){
+            RestProperty restProperty;
+            restProperty.sender_pro= hubConfig.code;
+            restProperty.latitude_pro= hubConfig.latitude;
+            restProperty.longitude_pro= hubConfig.longitude;
+            restProperty.TEMP_SENSOR_pro= TEMP_SENSOR;
+            restProperty.PM_SENSOR_pro= PM_SENSOR;
+            restProperty.mac_str_pro = hubConfig.macStr;
+            bool ok = saveData(envData,restProperty);
+            if (ok) { 
+              blink_type = 0; //everything ok, no blink
+              showStatus(ok);
+            } else {
+              blink_type = 2;
+            }
+            
+          } else{
+//            showStatus(false);
+            blink_type = 1;
+          }
+          Serial.println();
         }else{
-          showStatus(false);
+//          showStatus(false);
+          blink_type = 3;
         }
-        Serial.println();
-      }else{
-        showStatus(false);
       }
-      delay(600000);
+      if (blink_type==1) { //Wifi connection error
+        showStatus(true);
+        delay(1000);
+        showStatus(false);
+        delay(1000);
+      } else {
+        flash(blink_type);
+        delay(2000);
+      }
+//      delay(600000);
     }
   }
 }
 
+void flash(int numberOfFlash) {
+  for (int i=0; i< numberOfFlash; i++) {
+    showStatus(true);
+    delay(150);
+    showStatus(false);
+    delay(150);
+  }
+}
 
 void highInterrupt(){
   btnVoltage = analogRead(CONFIG_BTN);
@@ -131,7 +171,8 @@ void readSnifferData()
      
     //read 
     readTemperatureAndHumidity();
-    while(envData.temperature <= 0 || envData.humidity <= 0){
+    int attemp = 0; 
+    while((envData.temperature <= 0 || envData.humidity <= 0) && attemp++<3) { //try 3 time read
       Serial.println("DHT11 sensor doesn't work properly");
       readTemperatureAndHumidity();  
       delay(5000);
@@ -177,11 +218,11 @@ void readTemperatureAndHumidity(){
   }
 void showStatus(bool ok){
   if(ok){
-    //digitalWrite(OK_PIN,HIGH);
-    //digitalWrite(ERR_PIN,LOW);
+//    digitalWrite(OK_PIN,HIGH);
+    digitalWrite(ERR_PIN,HIGH);
   }else{
-    //digitalWrite(OK_PIN,LOW);
-    //digitalWrite(ERR_PIN,HIGH);
+//    digitalWrite(OK_PIN,LOW);
+    digitalWrite(ERR_PIN,LOW);
   }
 }
 
